3ed0ceac9647d834085b997ff88e2e2199950c23
Allysu <allysom.renczenczen18@gmail.com>
2023-04-10 13:32:57 -0300

    Pizzaria em java

---
+++ src/main/java/br/fag/aula01/Pizza.java
@@ -0,0 +1,8 @@
+package br.fag.aula01;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Pizza {
+
+}
---
+++ src/main/java/br/fag/aula01/Status.java
@@ -0,0 +1,9 @@
+package br.fag.aula01;
+
+public enum Status {
+    DISPONIVEL,
+    ANDAMENTO,
+    CONCLUIDO,
+    CANCELADO,
+}
+
