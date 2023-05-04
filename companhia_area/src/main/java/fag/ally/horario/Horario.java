ae8094bbe098029548293d215f757830f110128a
Allysom Renczenczen <82781674+AllysomFernando@users.noreply.github.com>
2023-05-04 20:39:58 -0300

    commit CompanhiaAerea

--- companhia_area/src/main/java/fag/ally/horario/Horario.java
+++ companhia_area/src/main/java/fag/ally/horario/Horario.java
@@ -1,5 +1,7 @@
 package fag.ally.horario;
 
+import fag.ally.transporte.Aviao;
+
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -19,6 +21,25 @@ public class Horario {
         this.dataChegada = dataChegada;
         this.classeVoo = classeVoo;
     }
+
+    public Horario() {
+
+    }
+
+
+    private static String gerarCodigo() {
+        Random geradorAleatorio = new Random();
+        return String.valueOf(geradorAleatorio.nextInt(100));
+    }
+
+    public static LocalDate gerarDataAleatoria() {
+        LocalDate dataInicial = LocalDate.of(2023, 1, 30);
+        LocalDate dataFinal = LocalDate.of(2023, 12, 30);
+        long dias = ChronoUnit.DAYS.between(dataInicial, dataFinal);
+        long diasAleatorios = ThreadLocalRandom.current().nextLong(dias + 1);
+        return dataInicial.plusDays(diasAleatorios);
+    }
+
 
     public String getCodigo() {
         return codigo;
@@ -50,28 +71,5 @@ public class Horario {
 
     public void setClasseVoo(String classeVoo) {
         this.classeVoo = classeVoo;
-    }
-
-    private static String gerarCodigo() {
-        Random geradorAleatorio = new Random();
-        return String.valueOf(geradorAleatorio.nextInt(100));
-    }
-
-    public static List<Horario> criarClasseVoo() {
-        List<Horario> tiposClasseVoo = new ArrayList<>();
-        tiposClasseVoo.add(new Horario(gerarCodigo(), LocalDate.of(2023, 1, 22), LocalDate.of(2023, 2, 12), "Economica"));
-        tiposClasseVoo.add(new Horario(gerarCodigo(), LocalDate.of(2023, 1, 22), LocalDate.of(2023, 2, 12), "Executiva"));
-        tiposClasseVoo.add(new Horario(gerarCodigo(), LocalDate.of(2023, 1, 22), LocalDate.of(2023, 2, 12), "Primeira-Classe"));
-
-
-        return tiposClasseVoo;
-    }
-
-    public static LocalDate gerarDataAleatoria() {
-        LocalDate dataInicial = LocalDate.of(2023, 1, 30);
-        LocalDate dataFinal = LocalDate.of(2023, 12, 30);
-        long dias = ChronoUnit.DAYS.between(dataInicial, dataFinal);
-        long diasAleatorios = ThreadLocalRandom.current().nextLong(dias + 1);
-        return dataInicial.plusDays(diasAleatorios);
     }
 }
--- companhia_area/src/main/java/fag/ally/transporte/Rota.java
+++ companhia_area/src/main/java/fag/ally/transporte/Rota.java
@@ -1,32 +1,30 @@
 package fag.ally.transporte;
 
+import fag.ally.horario.Horario;
+
+import java.util.Random;
 import java.util.Scanner;
 
 public class Rota {
-    static Scanner inputScanner = new Scanner(System.in);
     private String codigo;
     private String descricao;
-
-
     private String rota;
 
-    public Rota(String codigo, String descricao, String rota) {
-        this.codigo = codigo;
-        this.descricao = descricao;
-        this.rota = rota;
-    }
 
-
-
-    public String getPonte() {
+    public static Rota gerarRota() {
+        Random gerador = new Random();
+        Scanner scanner = new Scanner(System.in);
+        Rota rota = new Rota();
+        rota.setCodigo(String.valueOf(gerador.nextInt(1000)));
+        rota.setDescricao("Descrição da rota " + rota.getCodigo());
+        System.out.println("Digite a rota:");
+        rota.setRota(scanner.nextLine());
+        Horario horario = new Horario();
+        System.out.println("O codigo da sua data é: " + horario.getCodigo());
+        System.out.println("A data da usa partida é: " + horario.getDataPartida());
+        System.out.println("A data da sua chegada é: " + horario.getDataChegada());
         return rota;
     }
-
-    public void setPonte(String ponte) {
-        this.rota = ponte;
-    }
-
-
     public String getCodigo() {
         return codigo;
     }
@@ -43,7 +41,11 @@ public class Rota {
         this.descricao = descricao;
     }
 
+    public String getRota() {
+        return rota;
+    }
 
-
-
+    public void setRota(String rota) {
+        this.rota = rota;
+    }
 }
