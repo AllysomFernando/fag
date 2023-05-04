8f7895b32b9eaba5d46a9d1e815cb5da1b92cacf
Allysom Renczenczen <82781674+AllysomFernando@users.noreply.github.com>
2023-05-04 15:41:39 -0300

    commit CompanhiaAerea

--- companhia_area/src/main/java/fag/ally/Main.java
+++ companhia_area/src/main/java/fag/ally/Main.java
@@ -1,9 +1,7 @@
 package fag.ally;
 
-import fag.ally.bilhete.Bagagem;
-
 public class Main {
     public static void main(String[] args) {
-        Bagagem.colocarBagagem();
+        System.out.println("Hello world!");
     }
 }
