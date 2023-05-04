5f1f2387239084f47d373b369ca2efd5d4551834
Allysom Renczenczen <82781674+AllysomFernando@users.noreply.github.com>
2023-05-04 15:40:41 -0300

    commit CompanhiaAerea

--- companhia_area/src/main/java/fag/ally/bilhete/Bilhete.java
+++ companhia_area/src/main/java/fag/ally/bilhete/Bilhete.java
@@ -1,6 +1,49 @@
 package fag.ally.bilhete;
 
+import java.util.Random;
+import java.util.Scanner;
+
+
+
 public class Bilhete {
+
+    private static final Scanner inputScanner = new Scanner(System.in);
+
+
+
+
+
+    public static void comprar(){
+
+        int LeitorTipo;
+
+        Random gerador = new Random();
+
+        System.out.println("Qual vai ser o tipo de pagamento");
+
+        System.out.println("1 - Débito");
+        System.out.println("2 - Crédito");
+        System.out.println("3 - PIX");
+
+        LeitorTipo = inputScanner.nextInt();
+
+        System.out.println("Pagamento realizado!");
+
+        System.out.println("O numero do assento é: " + gerador.nextInt(40));
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
     private int numero;
     private String assento;
 
@@ -49,6 +92,5 @@ public class Bilhete {
     }
 
     private String situacao;
-
 
 }
